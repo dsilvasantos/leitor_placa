3ecd6c45ac70b3998d5b52411962008c2a7f7467
Diego Santos da Silva <dsilvasantos00@hotmail.com>
2025-05-07 23:48:19 -0300

    Ajuste bat de instalação

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Ativando ambiente virtual..."
-python3 -m venv venv
-source venv/bin/activate
-pip install -r app/requirements.txt
-
-echo "Iniciando API Flask..."
-cd app
-python api.py
