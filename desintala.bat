e7dc2897dc3217cf253fbb933f9f42d25d08ee87
Diego Santos da Silva <dsilvasantos00@hotmail.com>
2025-08-14 23:35:32 -0300

    Melhoria
    Ajuste inicialização

--- desintala.bat
+++ desintala.bat
@@ -1,15 +1,14 @@
 @echo off
 echo Desinstalando ambiente virtual...
+python -m venv venv
+call venv\Scripts\activate
 pip uninstall flask -y
 pip uninstall psycopg2-binary -y
 pip uninstall ultralytics -y
 pip uninstall requests -y
 pip uninstall pytesseract -y
-pip uninstall opencv-python-headless -y
-pip uninstall opencv-python -y
-pip uninstall opencv-python -y
-pip uninstall paddlepaddle-gpu -y
-pip uninstall paddleocr -y
 pip uninstall torch torchvision torchaudio -y
-
+pip uninstall opencv-python
+pip uninstall opencv-python-headless
+pip uninstall opencv-contrib-python
 
--- instala.bat
+++ instala.bat
@@ -2,7 +2,5 @@
 echo Ativando ambiente virtual...
 python -m venv venv
 call venv\Scripts\activate
-pip uninstall opencv-python-headless -y
 pip install -r app\requirements.txt
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pip install ultralytics
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
